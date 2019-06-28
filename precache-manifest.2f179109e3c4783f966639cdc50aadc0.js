fbe5fa910188546b38cb2562701bc8a9cbeda8ce
kamquatz <dennismuga@gmail.com>
2019-06-28 10:42:05 +0300

    Updates

---
+++ precache-manifest.2f179109e3c4783f966639cdc50aadc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c84c7ef5537c3d02caba98713420a38",
+    "url": "/mirror-brand/index.html"
+  },
+  {
+    "revision": "a6401a0afd8d4eb073d5",
+    "url": "/mirror-brand/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/mirror-brand/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a6401a0afd8d4eb073d5",
+    "url": "/mirror-brand/static/js/main.0fb0d7d6.chunk.js"
+  },
+  {
+    "revision": "b27583ac44a8cc3d3422",
+    "url": "/mirror-brand/static/js/runtime~main.3adb2d63.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mirror-brand/static/media/logo.5d5d9eef.svg"
+  }
+]);
